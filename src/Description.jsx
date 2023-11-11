5aef48e8e5781141e01f0119c6a4d1a4f67ed880
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2023-11-11 20:23:38 +0000

    first commit

---
+++ src/Description.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Description extends Component {
+  render() {
+    const { text } = this.props;
+    return <><span class="des">Fullstack dev &amp; UX UI</span></>;
+  }
+}
+
+export default Description;
+
+
+
+
+
+
+
+
---
+++ src/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Description from './Description';
+
+const Title = ({ text }) => {
+  return <>  
+    <div class="card-title">DIALLO<br></br>
+   {Description}
+</div>
+</>;
+};
+
+export default Title;
